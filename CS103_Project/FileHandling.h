cc7a63a836bc0761ff0cd08a2145b8f9afc698d4
Taihtrann270156691 <146798728+Taihtrann270156691@users.noreply.github.com>
2023-12-31 12:19:10 +1300

    Delete CS103_Project directory

--- CS103_Project/FileHandling.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "School.h"
-
-void fileReading(std::shared_ptr<std::map<short, School>> _schoolPtr);
-void splitLine(std::shared_ptr<std::map<short, School>> _schoolPtr, const std::string& line);
-void addSchool(const std::map<short, School>::iterator& it);
-void createNewFile(const std::map<short, School>::iterator& _school);
-void updateFile(const std::map<short, School>::iterator& _school);
